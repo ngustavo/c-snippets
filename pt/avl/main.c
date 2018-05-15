7ee6ceaf39567868a1bb4c0d099cfd554f80faf5
Gustavo N <gusantana1@gmail.com>
2018-05-15 15:20:43 -0300

    Create main.c

---
+++ pt/avl/main.c
@@ -0,0 +1,17 @@
+// UniCarioca - Profº Sergio Monteiro
+// APS - Arvores AVL
+// Estrutura de Dados - 331
+// Gustavo Santana
+// Felipe Monsores
+// Clayton Santos
+
+#include "aps.h"
+
+int main(void) {
+    
+    cabecalho();
+    
+    while(menu());
+    
+    return 0;
+}
